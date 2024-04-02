0d56a1ec8d7e278d1bb141267ab67313491ccf25
Gouiseong <rhdmltjd1324@gmail.com>
2024-04-02 10:50:41 +0900

    MUI 테마 커스터마이징

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,17 @@
-import "./globals.css";
-
+import './globals.css';
 
 export const metadata = {
-  title: "Main | TodoList",
-  description: "TodoList Management Service",
+  title: 'Main | TodoList',
+  description: 'TodoList Management Service',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* <section>lorem*20</section> */}
+      </body>
     </html>
   );
 }
